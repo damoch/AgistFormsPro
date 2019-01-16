9cba6a75be75befd3b69ad3ee9ece919f0fb148d
damoch <damochramowicz260@gmail.com>
2019-01-16 22:17:23 +0100

    Target Shape

---
+++ Assets/Scripts/Game/TargetShape.cs
@@ -0,0 +1,65 @@
+﻿using AgistForms.Assets.Scripts.Enums;
+using AgistForms.Assets.Scripts.Structs;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AgistForms.Assets.Scripts.Game
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class TargetShape : MonoBehaviour
+    {
+        public LevelController LevelController { get; set; }
+
+        [SerializeField]
+        private ShapeSprite[] _wrongShapes;
+
+
+        [SerializeField]
+        private ShapeSprite[] _rightShapes;
+
+        [SerializeField]
+        private ShapeType _targetType;
+
+        private Dictionary<ShapeType, Sprite> _rightShapesCache;
+        private Dictionary<ShapeType, Sprite> _wrongShapesCache;
+        private SpriteRenderer _spriteRenderer;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            PrepareSpriteCaches();
+            SetSprite(false);
+        }
+
+        private void SetSprite(bool isCorrect)
+        {
+            _spriteRenderer.sprite = isCorrect ? _rightShapesCache[_targetType] : _wrongShapesCache[_targetType];
+        }
+
+        private void PrepareSpriteCaches()
+        {
+            _rightShapesCache = new Dictionary<ShapeType, Sprite>();
+            _wrongShapesCache = new Dictionary<ShapeType, Sprite>();
+
+            foreach (var shape in _wrongShapes)
+            {
+                _wrongShapesCache.Add(shape.ShapeType, shape.Sprite);
+            }
+
+            foreach (var shape in _rightShapes)
+            {
+                _rightShapesCache.Add(shape.ShapeType, shape.Sprite);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var player = collision.gameObject.GetComponent<Player>();
+
+            if (player)
+            {
+                SetSprite(player.ShapeType == _targetType);
+            }
+        }
+    }
+}
