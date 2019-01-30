d1664c55e8be8d4160a35d9fd8e35364674ded71
damoch <damochramowicz260@gmail.com>
2019-01-30 21:05:27 +0100

    First draft of editor

---
+++ Assets/Scripts/LevelEditor/EditorObject.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace AgistForms.Assets.Scripts.LevelEditor
+{
+    public class EditorObject : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _originalObject;
+
+        private SpriteRenderer _spriteRenderer;
+        private BoxCollider2D _collider;
+        private Rigidbody2D _rigidbody;
+        private bool _isDragging;
+        private Camera _mainCamera;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<BoxCollider2D>();
+            _mainCamera = FindObjectOfType<Camera>();
+            SetOriginalRepresentation();
+        }
+
+        private void SetOriginalRepresentation()
+        {
+            transform.localScale = _originalObject.transform.localScale;
+            _spriteRenderer.sprite = _originalObject.GetComponent<SpriteRenderer>().sprite;
+            var col = _originalObject.GetComponent<BoxCollider2D>();
+            _collider.size = col.size;
+        }
+
+        private void Update()
+        {
+            if (!_isDragging) return;
+
+            var stp = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(stp.x, stp.y);
+        }
+
+        private void OnMouseDown()
+        {
+            _isDragging = true;
+        }
+
+        private void OnMouseUp()
+        {
+            _isDragging = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log(collision);
+        }
+    }
+}
